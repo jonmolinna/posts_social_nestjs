3b84fac9dea404869ca735952949bfdfaefd1c6e
jonmolinna <jmolina2624@gmail.com>
2024-10-14 09:00:44 -0500

    App

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { PostsModule } from './posts/posts.module';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://jmolina2624:FwqJqfE6dL4zo6Eg@cluster0.h8rkt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    ),
+    UsersModule,
+    AuthModule,
+    PostsModule,
+    CloudinaryModule,
+    BookmarkModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
